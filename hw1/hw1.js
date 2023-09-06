921971e3acd310c5f15c8e183e087adf6e35b19f
chrforb <chrforb@iu.edu>
2023-09-05 20:36:56 -0500

    9/5

--- hw1/hw1.js
+++ hw1/hw1.js
@@ -1,6 +1,7 @@
 //dating problem
 var tickets = 3;
 var cost = 14;
+
 var total = cost*tickets;
 
 //shopping problem
@@ -9,18 +10,29 @@ var shirt = 35;
 var pants = 75;
 var shoes = 60;
 
+var myTotal = shirt + pants + shoes;
+var newMoney = myMoney - myTotal;
+
+
 //pizza problem
 var pizzas = 4;
 var perStudent = 2.5;
 var slicePerPizza = 8;
+
+var totalSlice = pizzas*slicePerPizza
+var studentsFed = totalSlice / 2.5
 
 //monty's problem
 var adult = 12;
 var child = 6;
 var drink = 1.5;
 
+var montyTotal = 2*adult + child + 3*drink 
+
 //avg tips problem
 var w1 = 202.45;
 var w2 = 134.97;
 var w3 = 256.63;
 var w4 = 178.22;
+
+var avg = (w1+w2+w3+w4)/4
